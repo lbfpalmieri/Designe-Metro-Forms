6bee7e45a3f5bf336b427dd783dca7d50c3e64eb
Lucas Palmieri <lucas.palmieri@outlook.com>
2019-05-21 14:08:17 -0300

    Acrescentando Form frmDebit.

---
+++ LDV_DESIGNE_BZ/Forms/frmDebit.cs
@@ -0,0 +1,10 @@
+﻿namespace LDV_DESIGNE_BZ.Forms
+{
+    public partial class frmDebit : MetroFramework.Forms.MetroForm
+    {
+        public frmDebit()
+        {
+            InitializeComponent();
+        }
+    }
+}
